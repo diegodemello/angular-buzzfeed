1f6803a71ce0ebe35f883f75d6c380c31b60cf8d
Diego de Mello <diegodemello027@gmail.com>
2025-03-25 18:29:52 -0300

    Criando a lógica do resultado.

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -38,6 +38,7 @@ export class QuizComponent implements OnInit {
 
   playerChoice(value:string){
     this.answers.push(value);
+    this.nextSetp();
   }
 
   async nextSetp(){
@@ -45,8 +46,22 @@ export class QuizComponent implements OnInit {
     if(this.questionMaxIndex > this.questionIndex){
       this.questionSelected = this.questions[this.questionIndex];
     }else{
+      const finalAnswer:string = await this.checkResult(this.answers);
+
       this.finished = true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results];
     }
   }
 
+  checkResult(answers:string[]){
+    const result = answers.reduce((previous, current, i, arr)=>{
+      if(arr.filter(item => item === previous).length > arr.filter(item => item === current).length){
+        return previous;
+      }else{
+        return current;
+      }
+    })
+
+    return result;
+  }
 }
